74edfa51e321002f1211cff58c79f4bb1d307017
Vivaldi Carrillo <xxcitizen33xx@gmail.com>
2021-02-16 19:29:17 -0700

    Version 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import  React, { Component } from  'react';
+import { BrowserRouter } from  'react-router-dom'
+import { Route, Link } from  'react-router-dom'
+import  NotesList  from  './NotesList'
+import  NotesCreateUpdate  from  './NotesCreateUpdate'
+import  './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const  BaseLayout  = () => (
+  <div  className="container-fluid">
+      <nav  className="navbar navbar-expand-lg navbar-light bg-light">
+          <a  className="navbar-brand"  href="#">CRUD de Tareas</a>
+          <button  className="navbar-toggler"  type="button"  data-toggle="collapse"  data-target="#navbarNavAltMarkup"  aria-controls="navbarNavAltMarkup"  aria-expanded="false"  aria-label="Toggle navigation">
+          <span  className="navbar-toggler-icon"></span>
+      </button>
+      <div  className="collapse navbar-collapse"  id="navbarNavAltMarkup">
+          <div  className="navbar-nav">
+              <a  className="nav-item nav-link"  href="/">TAREAS</a>
+              <a  className="nav-item nav-link"  href="/customer">CREAR TAREA</a>
+          </div>
+      </div>
+      </nav>
+      <div  className="content">
+          <Route  path="/"  exact  component={NotesList}  />
+          <Route  path="/notes/:pk"  component={NotesCreateUpdate}  />
+          <Route  path="/notes/"  exact  component={NoteCreateUpdate}  />
+      </div>
+  </div>
+  )
+  //Hola mundo
 
-export default App;
+  class  App  extends  Component {
+
+    render() {
+        return (
+        <BrowserRouter>
+            <BaseLayout/>
+        </BrowserRouter>
+        );
+    }
+    }
+    export  default  App;
