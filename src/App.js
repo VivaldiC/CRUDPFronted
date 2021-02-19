468a4d4ab57d94f605e4e51ba8fc31412ee696f1
Vivaldi Carrillo <xxcitizen33xx@gmail.com>
2021-02-19 14:43:29 -0700

    Fronted Version 0.3 sin poder mostrar textfields

--- src/App.js
+++ src/App.js
@@ -1,41 +1,45 @@
-import  React, { Component } from  'react';
-import { BrowserRouter } from  'react-router-dom'
-import { Route, Link } from  'react-router-dom'
-import  NotesList  from  './NotesList'
-import  NotesCreateUpdate  from  './NotesCreateUpdate'
-import  './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 
-const  BaseLayout  = () => (
-  <div  className="container-fluid">
-      <nav  className="navbar navbar-expand-lg navbar-light bg-light">
-          <a  className="navbar-brand"  href="#">CRUD de Tareas</a>
-          <button  className="navbar-toggler"  type="button"  data-toggle="collapse"  data-target="#navbarNavAltMarkup"  aria-controls="navbarNavAltMarkup"  aria-expanded="false"  aria-label="Toggle navigation">
-          <span  className="navbar-toggler-icon"></span>
-      </button>
-      <div  className="collapse navbar-collapse"  id="navbarNavAltMarkup">
-          <div  className="navbar-nav">
-              <a  className="nav-item nav-link"  href="/">TAREAS</a>
-              <a  className="nav-item nav-link"  href="/customer">CREAR TAREA</a>
-          </div>
-      </div>
-      </nav>
-      <div  className="content">
-          <Route  path="/"  exact  component={NotesList}  />
-          <Route  path="/notes/:pk"  component={NotesCreateUpdate}  />
-          <Route  path="/notes/"  exact  component={NoteCreateUpdate}  />
-      </div>
+import  NotesList from './NotesList'
+import  NoteCreateUpdate  from './NoteCreateUpdate'
+import './App.css';
+
+const BaseLayout = () => (
+  <div className="container-fluid">
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="#">NOTES FRONTEND</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div className="navbar-nav">
+      <a className="nav-item nav-link" href="/">NOTES</a>
+      <a className="nav-item nav-link" href="/customer">CREATE NOTE</a>
+
+    </div>
   </div>
-  )
-  //Hola mundo
+</nav>
 
-  class  App  extends  Component {
+    <div className="content">
+      <Route path="/" exact component={NotesList} />
+      <Route path="/notes/:pk"  component={NoteCreateUpdate} />
+      <Route path="/notes/" exact component={NoteCreateUpdate} />
 
-    render() {
-        return (
-        <BrowserRouter>
-            <BaseLayout/>
-        </BrowserRouter>
-        );
-    }
-    }
-    export  default  App;
+    </div>
+
+  </div>
+)
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <BaseLayout/>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
--- src/NotesList.js
+++ src/NotesList.js
@@ -4,74 +4,74 @@ import  NotesService  from  './NotesService';
 
 const  notesService  =  new  NotesService();
 
-class NotesList extends Component{
+class  NotesList  extends  Component {
 
-    constructor(props){
-        super(props);
-        this.state = {
-            notes: [],
-            nextPageURL: ''
-        };
-        this.nextPage = this.nextPage.bind(this);
-        this.handDelete = this.handleDelete.bind(this);
-    }
+constructor(props) {
+    super(props);
+    this.state  = {
+        notes: [],
+        nextPageURL:  ''
+    };
+    this.nextPage  =  this.nextPage.bind(this);
+    this.handleDelete  =  this.handleDelete.bind(this);
+}
 
-    componentDidMount(){
-        var self = this;
-        notesService.getNotes().then(function(result){
-            self.setState({notes: result.data, nextPageURL: result.nextlink})
+componentDidMount() {
+    var  self  =  this;
+    notesService.getNotes().then(function (result) {
+        console.log(result);
+        self.setState({ notes:  result.data, nextPageURL:  result.nextlink})
+    });
+}
+handleDelete(e, id){
+    var  self  =  this;
+    notesService.deletenote({id :  id}).then(()=>{
+        var  newArr  =  self.state.notes.filter(function(obj) {
+            return  obj.id  !==  id;
         });
-    }
 
-    handDelete(){
-        var self = this;
-        notesService.deleteNote({pk : pk}).then(() => {
-            var newArr = self.state.notes.filter(function (obj){
-                return obj.pk !== pk;
-            });
-            self.setState({notes: newArr})
-        });
-    }
+        self.setState({notess:  newArr})
+    });
+}
 
-    nextPage(){
-        var  self  =  this;
-        notesService.getNotesByURL(this.state.nextPageURL).then((result) => {
-            self.setState({ notes:  result.data, nextPageURL:  result.nextlink})
-        });
-    }
+nextPage(){
+    var  self  =  this;
+    console.log(this.state.nextPageURL);
+    notesService.getNotesByURL(this.state.nextPageURL).then((result) => {
+        self.setState({ notes:  result.data, nextPageURL:  result.nextlink})
+    });
+}
+render() {
 
-    render() {
-
-        return (
+    return (
         <div  className="customers--list">
             <table  className="table">
-                <thead  key="thead">
-                <tr>
-                    <th>#</th>
-                    <th>Tittle</th>
-                    <th>Description</th>
-                    <th>Date</th>
-                </tr>
-                </thead>
-                <tbody>
-                    {this.state.notes.map( note  =>
-                    <tr  key={note.pk}>
-                        <td>{note.pk}  </td>
-                        <td>{note.tittle}</td>
-                        <td>{note.description}</td>
-                        <td>{note.date}</td>
-                        <td>
-                        <button  onClick={(e)=>  this.handleDelete(e,c.pk) }> Delete</button>
-                        <a  href={"/notes/" + c.pk}> Update</a>
-                        </td>
-                    </tr>)}
-                </tbody>
+            <thead  key="thead">
+            <tr>
+                <th>#</th>
+                <th>Note Tittle</th>
+                <th>Description</th>
+                <th>Date</th>
+                <th>Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+            {this.state.notes.map( c  =>
+                <tr  key={c.id}>
+                <td>{c.id}  </td>
+                <td>{c.task_title}</td>
+                <td>{c.description}</td>
+                <td>{c.createdAt}</td>
+                <td>
+                <button  onClick={(e)=>  this.handleDelete(e,c.id) }> Delete</button>
+                <a  href={"/note/" + c.id}> Update</a>
+                </td>
+            </tr>)}
+            </tbody>
             </table>
             <button  className="btn btn-primary"  onClick=  {  this.nextPage  }>Next</button>
         </div>
         );
-    }
-
+  }
 }
-
-export default NotesList;
+export  default  NotesList;
